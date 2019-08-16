e48cce28008ac34746a07f74a319c94f48baf801
Frank <frank@senso.ai>
2019-08-16 11:00:47 -0400

    getting list backend finished and adding env variables

--- app.py
+++ app.py
@@ -18,25 +18,25 @@ SURVEY_DATA['Asian'] = ['Japanese', 'Korean', 'Thai', 'Indian', 'Korean']
 @app.route('/menu/', methods=['POST'])
 def menu_page():
     try:
-        top_choices = request.get_json().get('top_food_choices', None)
-        recommended = request.get_json().get('food_recommended_by_your_friends', None)
-        menu_items = request.get_json().get('menu_items', None)
+        top_choices = request.get_json().get('top_food_choices', [])
+        recommended = request.get_json().get('food_recommended_by_your_friends', [])
+        menu_items = request.get_json().get('menu_items', [])
         top_choices_prices = []
         top_choices_total_price = 0
         for choice in top_choices:
-            price = menu.get(choice, '-99999$')
+            price = menu.get(choice, '10$')
             top_choices_prices.append(price)
             top_choices_total_price += float(price.rstrip('$'))
         recommended_prices = []
         recommended_total_price = 0
         for rec in recommended:
-            price = menu.get(rec, '-99999$')
+            price = menu.get(rec, '10$')
             recommended_prices.append(price)
             recommended_total_price += float(price.rstrip('$'))
         menu_items_prices =[]
         menu_items_total_price = 0
         for item in menu_items:
-            price = menu.get(item, '-99999$')
+            price = menu.get(item, '10$')
             menu_items_prices.append(price)
             menu_items_total_price += float(price.rstrip('$'))
         session['top_choices_prices'] = top_choices_prices
@@ -57,44 +57,50 @@ def menu_page():
 @app.route('/survey', methods=['POST'])
 def survey():
     try:
-        if "meal_type" in request.get_json():
-            session['meal_type'] = list(request.get_json().get('meal_type', None))
-        if "food_region" in request.get_json():
-            session['food_region'] = list(request.get_json().get('food_region', None))
-        if "food_type" in request.get_json():
-            session['food_type'] = list(request.get_json().get('food_type', None))
-        if "budget_value" in request.get_json():
-            session['budget_value'] = list(request.get_json().get('budget_value', None))
-
-        if (session.get('meal_type') is None) and (session.get('food_region') is None) and (session.get('food_type') is None) \
-                and (session.get('budget_value') is None):
-            # In case there's nothing set for session
-            session['meal_type'] = ['Vegetarian']
-            session['food_region'] = ['European']
-            session['food_type'] = ['Italian']
-            session['budget_value'] = ['$$$']
-
+        # if "meal_type" in request.get_json():
+        #     session['meal_type'] = list(request.get_json().get('meal_type', None))
+        # if "food_region" in request.get_json():
+        #     session['food_region'] = list(request.get_json().get('food_region', None))
+        # if "food_type" in request.get_json():
+        #     session['food_type'] = list(request.get_json().get('food_type', None))
+        # if "budget_value" in request.get_json():
+        #     session['budget_value'] = list(request.get_json().get('budget_value', None))
+        #
+        # if (session.get('meal_type') is None) and (session.get('food_region') is None) and (session.get('food_type') is None) \
+        #         and (session.get('budget_value') is None):
+        #     # In case there's nothing set for session
+        #     session['meal_type'] = ['Vegetarian']
+        #     session['food_region'] = ['European']
+        #     session['food_type'] = ['Italian']
+        #     session['budget_value'] = ['$$$']
+        data = {
+            'meal_type': list(request.get_json().get('meal_type', 'Vegetarian')),
+            'food_region': list(request.get_json().get('food_region', "European")),
+            'food_type': list(request.get_json().get('food_type', "Italian")),
+            'budget_value': list(request.get_json().get('budget_value', "$$$"))
+        }
+        list_recommendations(data)
         return jsonify({"success": True})
     except:
         traceback.print_exc()
         return jsonify({'success': False})
 
 
-@app.route('/recommend', methods=['GET'])
-def list_recommendations():
+# @app.route('/recommend', methods=['GET'])
+def list_recommendations(data):
     """Based on the session variable values, including survey results, bank balances, get lists
     of restaurants from Google Places API, and combine & return the results"""
     gps = GooglePlaceService()
     # Step 1
     # Pulling in the survey results, and turn them into search queries
-    base_restaurant_type = session['food_region'][0] + " restaurant"
-    query_strings = [meal_type + " " + base_restaurant_type for meal_type in session['meal_type']]
+    base_restaurant_type = data['food_region'][0] + " restaurant"
+    query_strings = [meal_type + " " + base_restaurant_type for meal_type in data['meal_type']]
     extended_query_strings = []
-    for food_type in session['food_type']:
+    for food_type in data['food_type']:
         extended_query_strings = extended_query_strings + [query + " " + food_type for query in query_strings]
 
     final_query_strings = []
-    for budget_value in session['budget_value']:
+    for budget_value in data['budget_value']:
         if budget_value in ["$", "$$"]:
             final_query_strings = final_query_strings + [query + " " + "cheap" for query in extended_query_strings]
         elif budget_value in ['$$$']:
@@ -104,19 +110,27 @@ def list_recommendations():
     total_results = []
     for query in final_query_strings:
         total_results = total_results + gps.search(query)
-        break  # Just to check and make sure we have the data format correctly
+        # break  # Just to check and make sure we have the data format correctly
 
     print(len(total_results))
     print(total_results[0])
     # Step 2
     # Parse the results and get an idea
+    final_results = []
+    for item in total_results:
+        item_list = item['results']
+        restaurant_details = dict()
+        for restaurant in item_list:
+            restaurant_details["formatted_address"] = restaurant["formatted_address"]
+            restaurant_details['latitude'] = restaurant['geometry']['location']['lat']
+            restaurant_details['longitude'] = restaurant['geometry']['location']['lng']
+            restaurant_details['photo_url'] = gps.get_photos(restaurant['photos']['photo_reference'])
+            restaurant_details['price_level'] = restaurant['price_level']
+            restaurant_details['rating'] = restaurant['rating']
+            restaurant_details['num_ratings'] = restaurant['user_ratings_total']
+        final_results = final_results + [restaurant_details]
 
-    # Step 3
-    # Combine the list of restaurant results
-
-    # Step 4
-    # Regeneration
-    return jsonify({"success": True})
+    return jsonify({"success": True, "results": final_results})
 
 
 @app.route('/test', methods=['GET'])
--- services/GooglePlaceService.py
+++ services/GooglePlaceService.py
@@ -1,19 +1,20 @@
 """Service class: wrapper for Google Places queries, especially text search"""
 import requests
 import urllib.parse
-
+import shutil
+from google.cloud import storage
+from settings import *
 
 class GooglePlaceService:
 
-    string = "ttAIzaSyDswjDbx2Jp4K"
-    string2 = "l92XkldtS2D2N0URGBlVott"
     latitude = 43.6404
     longitude = -79.3995
-    string = string + string2
-    string = string.split('tt')[1]
+
     base_url = "https://maps.googleapis.com/maps/api/place/textsearch/json?key={}&location={},{}&".format(
-        string, latitude, longitude)
+        API_KEY, latitude, longitude)
     photo_url = "https://maps.googleapis.com/maps/api/place/photo?"
+    client_id = 'YOUR CLIENT ID'
+    client_secret = 'YOUR CLIENT SECRET'
 
     def search(self, query):
         full_url = self.base_url + urllib.parse.urlencode({"query": query})
@@ -23,8 +24,15 @@ class GooglePlaceService:
         print(response.content)
         return response.json()
 
-    def get_photos(self, photo_reference):
-        params = {"key": self.string, "photo_reference": photo_reference, "maxheight": 150}
+    def get_photos(self, name, photo_reference):
+        params = {"key": API_KEY, "photo_reference": photo_reference, "maxheight": 150}
         full_url = self.photo_url + urllib.parse.urlencode(params)
-        response = requests.get(url=full_url)
-        return response.json()
+        response = requests.get(url=full_url, stream=True)
+        with open('{}.png'.format(name), 'wb') as out_file:
+            shutil.copyfileobj(response.raw, out_file)
+        storage_client = storage.Client()
+        bucket_name = 'foodar'
+        bucket = storage_client.get_bucket(bucket_name)
+        blob = bucket.blob('{}.png'.format(name))
+        blob.upload_from_filename('{}.png'.format(name))
+        return blob.generate_signed_url()
