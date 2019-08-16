63639f2d2f2869484bbc4b9250b654b2adc4c917
Frank <frank@senso.ai>
2019-08-16 00:49:15 -0400

    google place api added

--- services/GooglePlaceService.py
+++ services/GooglePlaceService.py
@@ -1,1 +1,13 @@
 """Service class: wrapper for Google Places queries, especially text search"""
+import requests
+import urllib.parse
+
+
+class GooglePlaceService:
+    string = "AIzaSyCoAfxtpSsCGFcR2WxQDCeWqO9BOFFr3xs"
+    base_url = "https://maps.googleapis.com/maps/api/place/textsearch/jkey={}&son?query=".format(string)
+
+    def search(self, query):
+        full_url = self.base_url + urllib.parse.urlencode({"query": query})
+        response = requests.get(url=full_url)
+        return response.json()
