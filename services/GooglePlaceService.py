7d7f41cce36841ecf472b78cdd7f2b8bdc75d40a
Frank <frank@senso.ai>
2019-08-16 06:52:19 -0400

    gps

--- services/GooglePlaceService.py
+++ services/GooglePlaceService.py
@@ -4,9 +4,10 @@ import urllib.parse
 
 
 class GooglePlaceService:
-    string = "AIzaSyCoAfxtpSsCGFcR2WxQDCeWqO9BOFFr3xs"
+    string = "ttAIzaSyCoAfxtpSsCGFcR2WxQDCeWqO9BOFFr3xstt"
     latitude = 43.6404
     longitude = -79.3995
+    string = string.split('tt')[1]
     base_url = "https://maps.googleapis.com/maps/api/place/textsearch/jkey={}&location={},{}&son?query=".format(
         string, latitude, longitude)
 
